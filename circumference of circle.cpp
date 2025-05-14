33666baed4508ae0aeb589f02127a8982555cbf1
Vaibhav Bhadoriya <Vbhadoriya78@gmail.com>
2025-05-14 10:01:23 +0530

    Add files via upload

---
+++ circumference of circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+// circumference of a circle 
+int main()
+{
+    float r;
+    cout<<"Enter your radius =";
+    cin>>r;
+    float c=2*3.14*r;
+    cout<<c<<"cm";
+    return 0;
+}
